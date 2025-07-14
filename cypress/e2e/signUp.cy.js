6f19bfb4e453e57af7c8f9272d558cfe04344d42
Piotr Kruszelnicki <arbuzzz1337@gmail.com>
2025-07-14 18:12:13 +0200

    intro adv solution

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,25 @@
-describe('Sign Up page', () => {
-  it('should provide an ability to register new account', () => {
+/// <reference types="cypress" />
 
+import { generateUser } from '../support/generate';
+
+describe('Sign Up page', () => {
+  before(() => {
+    cy.visit('https://conduit.mate.academy/');
+  });
+  it('should provide an ability to register new account', () => {
+    const { email, password, username } = generateUser();
+
+    cy.contains('a', 'Sign up')
+      .click();
+    cy.get('[placeholder="Username"]')
+      .type(username);
+    cy.get('[placeholder="Email"]')
+      .type(email);
+    cy.get('[placeholder="Password"]')
+      .type(password);
+    cy.contains('button', 'Sign up')
+      .click();
+    cy.get(`a.nav-link[href="/profile/${username.toLowerCase()}"]`)
+      .should('be.visible');
   });
 });
---
+++ cypress/support/generate.js
@@ -0,0 +1,14 @@
+import { faker } from '@faker-js/faker';
+
+function generateUser() {
+  const randomNumber = Math.random().toString().slice(2, 6);
+  const rawUsername = faker.internet.userName();
+  const cleanUsername = rawUsername.replace(/[^a-zA-Z0-9_]/g, '');
+  const username = `${cleanUsername}_${randomNumber}`;
+  const email = `${username}@mail.com`;
+  const password = '12345Qwert!';
+
+  return { email, password, username };
+}
+
+module.exports = { generateUser };
